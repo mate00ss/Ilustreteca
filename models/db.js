81554b7f628be0385e13ba7b3bb3c731550ec959
Mateus Silva <mateussilvap11@gmail.com>
2023-04-26 11:15:41 -0300

    Base da tela inicial concluída
    Co-Authored-By: milkFighter <125591738+milkFighter@users.noreply.github.com>

---
+++ models/db.js
@@ -0,0 +1,4 @@
+import { Sequelize, DataTypes } from "sequelize";
+const sql = new Sequelize("site", "root", "root123",{
+    
+})
--- public/js/main.js
+++ public/js/main.js
@@ -1,35 +1,44 @@
 // Menu de opções //
 let menu_button = document.querySelector('#nav-button');
 let opt_buttons_div = document.querySelector('.opt-buttons-div');
+let atalho = document.querySelector('.atalho')
 
 menu_button.addEventListener('click', function() {
     if (menu_button.style.height != "100%") {
-
+        //AUMENTANDO O TAMANHO DO BOTÃO NA NAV
         menu_button.style.height = "100%"
         menu_button.style.width = "100%"
         menu_button.style.borderRadius = "0"
-
+        
+        //ABRINDO O MENU DE OPÇÕES
         opt_buttons_div.style.display = "flex"
-        opt_buttons_div.style.animation = "opt-buttons-div-open .5s ease 0s 1 forwards";
+        opt_buttons_div.style.animation = "opt-buttons-div-open .8s easeInOutQuart 0s 1 forwards";
+
+        //DIMINUINDO A DIV DE CONTEÚDO DO SITE
+        atalho.style.margin = "1rem 1rem 1rem 15rem"
+        atalho.style.width = "calc(100% - 16rem)"
     }
     else {
+        //DIMINUINDO O TAMANHO DO BOTÃO DA NAV
         menu_button.style.height = "3rem"
         menu_button.style.width = "10rem"
         menu_button.style.borderRadius = ".4rem"
-
-        opt_buttons_div.style.animation = "opt-buttons-div-close .5s ease 0s 1 forwards";
         
+        //FECHANDO O MENU DE OPÇÕES
+        opt_buttons_div.style.animation = "opt-buttons-div-close .8s easeInOutQuart 0s 1 forwards";
+
+        //AUMENTANDO A DIV DE CONTEÚDO DO SITE
+        atalho.style.margin = "1rem 1rem 1rem 1rem"
+        atalho.style.width = "calc(100% - 2rem)"
     }
 })
 
-// Troca de Tema //
-
+// Troca de Tema
 let nav = document.querySelector('nav');
 let body = document.querySelector('body');
 let theme_button = document.querySelector('.theme-changer');
 
 
 theme_button.addEventListener('click', function() {
-    nav.style.transition = "1s"
-    nav.style.backgroundColor = "#f00"
+    nav.style.backgroundColor = "#f0f"
 })
