40e4a5ad32bdb715f833ea637e86e6e7928098e6
ruansslv <antonioruandesousa8@gmail.com>
2023-05-23 12:13:26 -0300

    Porta

--- models/db.js
+++ models/db.js
@@ -4,12 +4,10 @@ var user = 'root';
 var password = 'root123';
 var host = 'localhost';
 var dialect = 'mariadb';
-var port = 9969;
 
 const sql = new Sequelize(database, user, password, {
     host: host,
     dialect: dialect,
-    port: port,
 });
 
 module.exports = sql
